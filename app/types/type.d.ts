06b93651cb98a978f1dbedcedc2202e56e95923a
rohilrajkarki <rohilrajkarki@gmail.com>
2024-12-17 14:11:50 +0800

    Added types for rider

--- app/types/type.d.ts
+++ app/types/type.d.ts
@@ -53,6 +53,26 @@ declare interface Ride {
   };
 }
 
+declare interface Rider {
+  origin_address: string;
+  destination_address: string;
+  origin_latitude: number;
+  origin_longitude: number;
+  destination_latitude: number;
+  destination_longitude: number;
+  ride_time: number;
+  fare_price: number;
+  payment_status: string;
+  driver_id: number;
+  user_id: string;
+  created_at: string;
+  driver: {
+    first_name: string;
+    last_name: string;
+    car_seats: number;
+  };
+}
+
 declare interface ButtonProps extends TouchableOpacityProps {
   title: string;
   bgVariant?: "primary" | "secondary" | "danger" | "outline" | "success";
